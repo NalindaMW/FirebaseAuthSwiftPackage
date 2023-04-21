2cd63477fae22a126b79e51665f3a8b1e7a6390f
Nalinda W <mastersilicon@gmail.com>
2023-04-21 15:18:08 +0530

    Update FirebaseAuthManager.swift

--- Sources/SwiftPackage/FirebaseAuthManager.swift
+++ Sources/SwiftPackage/FirebaseAuthManager.swift
@@ -2,6 +2,8 @@ import Firebase
 
 public class FirebaseAuthManager {
     
+    public init() {}
+    
     public func signIn(with email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             NetworkManger.sendWelcomeEmail(to: email)
